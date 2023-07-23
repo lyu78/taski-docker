d0f690a9f406eb4c1a0032794e0c0ebc45270bff
Иван Иванович <ivan@ivan.ru>
2023-07-23 20:06:00 +0300

    Add allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '84.201.153.84', 'lyubim-taski.ddns.net']
 
 
 # Application definition
